e765e7d209f2b8b16723f1ae8068f17746ad5e0a
John Wickerson <johnwickerson@cantab.net>
2017-02-07 14:56:32 +0000

    fixing symlinks

--- comparator/do_compare.sh
+++ comparator/do_compare.sh
@@ -1,15 +1,16 @@
 ALS_FILE=$1
 CMD=0
+cd ..
+
 export SOLVER=glucose
 
 echo "Alloy started at `date -u`."
 STAMP=`date +%y%m%d-%H%M%S`
 
 # run alloy
-cd ..
 XMLDIR=xml/$STAMP
 mkdir -p $XMLDIR
-ln -s -f $XMLDIR xml/_latest
+cd xml; rm -f _latest; ln -s $STAMP _latest; cd ..
 
 cd alloystar
 ./runalloy_once.sh ../comparator/$ALS_FILE $CMD ../$XMLDIR
@@ -29,20 +30,20 @@ fi
 echo "Converting XML file(s) to DOT file(s)."
 DOTDIR=dot/$STAMP
 mkdir -p $DOTDIR
-ln -s -f $DOTDIR dot/_latest
+cd dot; rm -f _latest; ln -s $STAMP _latest; cd ..
 gen/gen -Tdot -o $DOTDIR/exec.dot $XMLDIR/test_0.xml
 
 # convert .dot to .png
 echo "Converting DOT file(s) to PNG file(s)."
 PNGDIR=png/$STAMP
 mkdir -p $PNGDIR
-ln -s -f $PNGDIR png/_latest
+cd png; rm -f _latest; ln -s $STAMP _latest; cd ..
 dot -Tpng -o $PNGDIR/exec.png $DOTDIR/exec.dot
 
 # visualise .png
 if [ "$OS" = "x86-mac" ]
 then
-    open $PNGDIR/_latest
+    open png/_latest
 else
-    echo "Solution(s) are in $PNGDIR/_latest."
+    echo "Solution(s) are in png/_latest."
 fi
