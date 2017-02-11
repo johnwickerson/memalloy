3a444fcddce0ba0eb48cd5f56754bfa79044dc0e
John Wickerson <johnwickerson@cantab.net>
2017-02-11 18:06:31 +0000

    more work on CI

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,18 +7,18 @@
 OPAM_DEPENDS="xml-light"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
 4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.01.0,1.2.1) ppa=avsm/ocaml41+opam12 ;;
+4.02.1,1.1.2) ppa=avsm/ocaml42+opam11 ;;
+4.02.3,1.2.2) ppa=avsm/ocaml42+opam12 ;;
+4.02.1,1.2.0) ppa=avsm/ocaml42+opam120 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers opam
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
