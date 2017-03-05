8b94847592dfca1e58b24796b807ef3db44f0305
John Wickerson <johnwickerson@cantab.net>
2017-03-05 11:53:56 +0000

    Removing some old .als tests, replacing them with the new-style .sh tests.

--- Makefile
+++ Makefile
@@ -13,6 +13,12 @@ moretests:
 	@ tests/Q2_c11_simp_orig.sh
 	@ tests/Q2_c11_simp_orig2.sh
 	@ tests/Q2_sc_c11nodrf.sh
+	@ tests/Q2_c11_repairing0.sh
+	@ tests/Q2_c11_repairing1.sh
+	@ tests/Q2_c11_repairing2.sh
+	@ tests/Q2_c11_repairing3.sh
+	@ tests/Q2_c11_repairing4.sh
+	@ tests/Q2_c11_repairing5.sh
 	@ tests/Q4_c11_ppc.sh
 	@ tests/Q2_c11_lidbury_partial_iter.sh
 	@ tests/Q2_c11_sra_simp_iter.sh
---
+++ tests/Q2_c11_repairing0.sh
@@ -0,0 +1,10 @@
+./comparator \
+    -desc "Showing that Batty et al's POPL'16 C11 model is stronger than version 1 of Lahav et al.'s PLDI'17 model." \
+    -violates models/c11_repairing0.cat \
+    -satisfies models/c11_repairing1.cat \
+    -arch C \
+    -events 5 \
+    -expect 1 \
+    -satisfies models/c11_noscfences.cat \
+    -satisfies models/c11_normws.cat \
+    -satisfies models/totalsb.cat
