87e0502f5be3a780ad816c01f4d29537822214bb
John Wickerson <johnwickerson@cantab.net>
2017-05-09 10:01:52 +0100

    fixing comparator script again

--- top/comparator.py
+++ top/comparator.py
@@ -106,14 +106,13 @@ def main(argv=None):
     return 1
   nsolutions = len([x for x in os.listdir(xml_result_dir) if is_xml_file(x)])
   print "Alloy found %d solutions" % nsolutions
-
-  if args.expect:
-    if args.expect != nsolutions:
-      print "ERROR: Expected %d unique solutions, found %d" % (args.expect, nsolutions)
-      return 1
   
   if nsolutions == 0:
-    return 0
+    if args.expect and args.expect != 0:
+      print "ERROR: Expected %d unique solutions, found 0" % args.expect
+      return 1
+    else:
+      return 0
   
   print "Remove duplicates"
   code = remove_dups.main(args)
@@ -162,6 +161,11 @@ def main(argv=None):
     else:
       subprocess.Popen(["open", os.path.join(png_result_dir)])
 
+  if args.expect:
+    if args.expect != nsolutions:
+      print "ERROR: Expected %d unique solutions, found %d" % (args.expect, nsolutions)
+      return 1
+      
   return 0
 
 if __name__ == '__main__':
