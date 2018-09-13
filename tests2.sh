d03bc0c3a843b3df3d119324bb21865f7bc0b4cf
John Wickerson <johnwickerson@cantab.net>
2018-09-13 11:52:06 +0100

    tweaking regression test

--- tests2.sh
+++ tests2.sh
@@ -1,1 +1,1 @@
-./comparator -desc "adding_txns_arm8_4ev_allowset" -arch ARM8 -violates models/aarch64_txn.cat -fencerels -events 4 -iter -expect 201 -allowset
+./comparator -desc "adding_txns_arm8_4ev_allowset" -arch ARM8 -violates models/aarch64_txn.cat -fencerels -events 4 -iter -expect 196 -allowset
