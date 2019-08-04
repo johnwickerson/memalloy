569c63d8c2c82ddb8420c0d19ccf0db00ba6cd7b
Michalis Pardalos <m.pardalos@gmail.com>
2019-08-04 21:49:24 +0100

    Make a separate file for sql tests

---
+++ sqltests.sh
@@ -0,0 +1,2 @@
+./comparator -desc "postgres_not_sz_order" -arch SQL -violates models/sql16_sz_order.als -satisfies models/sql_postgres.als -events 6 -expect 0
+./comparator -desc "postgres_not_no_sz" -arch SQL -violates models/sql16_no_sz.als -satisfies models/sql_postgres.als -events 6 -expect 0
